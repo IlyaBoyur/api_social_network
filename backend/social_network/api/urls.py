1ec2addaf24e08da0e2e94dcba0d4f7a4e22dbeb
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-20 12:56:59 +0300

    Add urls

--- backend/social_network/api/urls.py
+++ backend/social_network/api/urls.py
@@ -1,1 +1,16 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .views import PostViewSet, register, user
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('user/', user, name='user'),
+    path('', include(router.urls)),
+
+]
