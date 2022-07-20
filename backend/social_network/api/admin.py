21a239a5bea4c306e42dfe8a3ecd744320e893f4
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-20 13:09:19 +0300

    Add admin panel

--- backend/social_network/api/admin.py
+++ backend/social_network/api/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+
+from .models import Post, PostImage
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'author', 'pub_date',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'post', 'image',)
+    search_fields = ('post',)
