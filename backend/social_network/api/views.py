577a3fcf88427124254286ae1dc4ced601a55579
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-20 12:59:15 +0300

    Add register and login views

--- backend/social_network/api/views.py
+++ backend/social_network/api/views.py
@@ -1,2 +1,29 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Post, User
+from .serializers import PostSerializer, UserSerializer
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user(request):
+    user = get_object_or_404(User, username=request.user.username)
+    return Response(UserSerializer(user).data)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    user.is_active = True
+    user.save()
+    return Response({'token': str(refresh.access_token)},
+                    status=status.HTTP_200_OK)
 
