13b7ea2ac527ac1aaeb438a78956e1c2b34af288
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-21 13:21:51 +0300

    Add token refresh endpoint

--- backend/social_network/api/urls.py
+++ backend/social_network/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import PostViewSet, register, user
 
@@ -10,6 +11,7 @@ router.register('posts', PostViewSet, basename='posts')
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/', user, name='user'),
     path('', include(router.urls)),
 
